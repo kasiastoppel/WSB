522b6968d1d5c5d5209b32cbe9f14d39d48bfec0
Kasia Stoppel <katarzynastoppel@wp.pl>
2023-03-27 21:58:50 +0200

    Initial commit

---
+++ WSB_project/git.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int add(int,int);
+
+int main() {
+    int a;
+    int b;
+
+    cout << "Wprowadź liczbę rzeczywistą a " << endl;
+    cin >> a;
+    cout << "Wprowadź liczbę rzeczywistą b " << endl;
+    cin >> b;
+
+    cout << a << "+" << b << "=" << a+b << endl;
+   
+
+
+
+    return 0;
+}
