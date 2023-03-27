dd401ee1e57a4bfbf79fc77f7f32a17644cb327e
Kasia Stoppel <katarzynastoppel@wp.pl>
2023-03-27 22:52:22 +0200

    Initial commit

---
+++ WSB_project/src/code.cpp
@@ -0,0 +1,5 @@
+#include "header.hpp"
+
+int add(int a, int b){
+	return a+b;
+}
---
+++ WSB_project/src/main.cpp
@@ -0,0 +1,18 @@
+#include "header.hpp"
+
+int main() {
+    int a;
+    int b;
+
+    cout << "Wprowadź liczbę rzeczywistą a " << endl;
+    cin >> a;
+    cout << "Wprowadź liczbę rzeczywistą b " << endl;
+    cin >> b;
+
+    cout << "Wynik dodawania to "<< add(a,b) << endl;
+   
+
+
+
+    return 0;
+}
